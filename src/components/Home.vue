1e43cae57c1b9cbde262960cba27b367d491a7e3
Your Name <you@example.com>
2020-08-26 22:09:02 +0800

    fix

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <Learn-header></Learn-header>
+    </div>
+</template>
+<script>
+import LearnHeader from './learn/Header'
+export default {
+  name: 'Home',
+  components: {
+    LearnHeader
+  }
+}
+</script>
+<style></style>
---
+++ src/components/learn/Header.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class='header'></div>
+</template>
+<script>
+export default {
+  name: 'LearnHeader'
+}
+</script>
+<style lang="less" scoped>
+  .header{
+    height: 50px;
+    background-color: red;
+    width:100%
+  }
+</style>
